846d57f303a476f2d865f473ea0c3da761260cb8
bongjun.sah <bongjun.sah@kt.com>
2023-03-30 17:33:55 +0900

    WebFlux MVC Pattern guide

--- src/main/java/kt/reactive/mywebflux/controller/R2CustomerController.java
+++ src/main/java/kt/reactive/mywebflux/controller/R2CustomerController.java
@@ -1,17 +1,20 @@
 package kt.reactive.mywebflux.controller;
 
 import kt.reactive.mywebflux.entity.Customer;
+import kt.reactive.mywebflux.exception.CustomAPIException;
 import kt.reactive.mywebflux.repository.R2CustomerRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 import java.time.Duration;
 
 @RestController
-@RequestMapping("/r2customers") public class R2CustomerController {
+@RequestMapping("/r2customers")
+public class R2CustomerController {
     private final R2CustomerRepository customerRepository;
     private final Sinks.Many<Customer> sinksMany;
     public R2CustomerController(R2CustomerRepository customerRepository) { this.customerRepository = customerRepository;
@@ -24,5 +27,32 @@ import java.time.Duration;
     @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Customer> findAllCustomers() {
         return customerRepository.findAll().delayElements(Duration.ofSeconds(1)).log();
+    }
+
+    @GetMapping("/sse")
+    public Flux<ServerSentEvent<Customer>> findAllCustomerSSE() {
+        return sinksMany.asFlux()
+                .mergeWith(customerRepository.findAll())
+                .map(customer -> ServerSentEvent.builder(customer).build())
+                .doOnCancel(() -> sinksMany.asFlux().blockLast());
+    }
+
+    @PostMapping
+    public Mono<Customer> saveCustomer(@RequestBody Customer customer) {
+        //tryEmitNext : Try emitting a non-null element, generating an onNext signal.
+        return customerRepository.save(customer).doOnNext(savedCustomer -> sinksMany.tryEmitNext(savedCustomer)).log();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Customer> findCustomerById(@PathVariable Long id) {
+        return customerRepository.findById(id)
+                .switchIfEmpty(Mono.error(
+                        new CustomAPIException("Customer Not Found with id " + id, HttpStatus.NOT_FOUND) )
+                );
+    }
+    @GetMapping("/name/{lastName}")
+    public Flux<Customer> findCustomerByName(@PathVariable String lastName){
+        return customerRepository.findByLastName(lastName) .switchIfEmpty(Mono.error(
+                new CustomAPIException("Customer Not Found with lastName " + lastName, HttpStatus.NOT_FOUND) ));
     }
 }
